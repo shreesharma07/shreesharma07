af38f64e2ca9a92b6bce6121e198a3b475770923
Shree Kumar Sharma <shrikumarsharma1998@gmail.com>
2022-12-09 14:43:30 +0530

    New Page Changes

--- Script/script.js
+++ Script/script.js
@@ -13,13 +13,37 @@ toggleSwitch.addEventListener("change", (e) => {
     }
 });
 
-
 // ! // Importing Scroll Motion Library //
 import LocomotiveScroll from "https://cdn.skypack.dev/locomotive-scroll";
 
 const scroller = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
 });
 
 // console.log({scroller});
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
